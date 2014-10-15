ee14d56872aed24b97a08469cf8a5a746d07efb0
hyunahpark <phablue@gmail.com>
2014-10-14 19:34:25 -0500

    make route for getting index page

--- app.js
+++ app.js
@@ -17,5 +17,14 @@ app.use(app.Router());
 app.use(bodyParser());
 
 http.createServer(app).listen(52273, function () {
-  console.log("Server Running at http://127.0.0.1:52273");
+  console.log('Server Running at http://127.0.0.1:52273');
+});
+
+app.get('/', function (req, res) {
+  fs.readFile('index.html', 'utf8', function (err, data) {
+    if (err) throw err;
+    client.query('select * from products;', function (err, result) {
+      res.send(data, ejs.render({data: result}));
+    });
+  });
 });
