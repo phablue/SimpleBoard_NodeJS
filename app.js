eedd5210a2afa2b16528257e29b74f6168f365d1
hyunahpark <phablue@gmail.com>
2014-10-15 10:44:34 -0500

    delete css

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var ejs = require('ejs');
 var fs = require('fs');
+var ejs = require('ejs');
+var mysql = require('mysql');
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+
+var dir = __dirname + '/view/';
 
 var client = mysql.createConnection({
   user: 'root',
@@ -13,7 +15,7 @@ var client = mysql.createConnection({
 
 var app = express();
 
-app.use(app.Router());
+app.use(express.Router());
 app.use(bodyParser());
 
 http.createServer(app).listen(52273, function () {
@@ -21,10 +23,10 @@ http.createServer(app).listen(52273, function () {
 });
 
 app.get('/', function (req, res) {
-  fs.readFile('index.html', 'utf8', function (err, data) {
+  fs.readFile(dir+'index.html', 'utf8', function (err, data) {
     if (err) throw err;
-    client.query('select * from products;', function (err, result) {
-      res.send(data, ejs.render({data: result}));
+    client.query('select * from products', function (err, result) {
+      res.send(ejs.render(data, {data: result}));
     });
   });
 });
